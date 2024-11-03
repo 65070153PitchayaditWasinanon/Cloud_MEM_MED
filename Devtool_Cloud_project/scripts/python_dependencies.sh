21ef3d6f51ed77a6dde320dbc6955d01d03ae728
65070153 Pitchayadit Wasinanon (นายพิชญดิฏฐ์ วศินานนท์) <pitchayaditwasinanon@gmail.com>
2024-11-04 02:09:57 +0700

    AWS TEST 5

--- Devtool_Cloud_project/scripts/python_dependencies.sh
+++ Devtool_Cloud_project/scripts/python_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PROJECT_MAIN_DIR_NAME="Devtool_Cloud_project"
+PROJECT_MAIN_DIR_NAME="Cloud_MEM_MED/Devtool_Cloud_project"
 
 # Validate variables
 if [ -z "$PROJECT_MAIN_DIR_NAME" ]; then
